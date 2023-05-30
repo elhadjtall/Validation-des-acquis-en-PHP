7dffc11b32b534d25c4107efe2623c53a9ed7fd2
unknown <elhadjoumartall10@gmail.com>
2023-05-30 17:48:24 +0200

    Verification du solde + modif

--- classes/compte.php
+++ classes/compte.php
@@ -6,12 +6,22 @@ class Compte {
 
     public function __construct($nom, $solde){
         $this->nom = $nom;
-        $this->solde = $solde;
+        // On va controler l'affection à la propriété du solde
+        if(is_numeric($solde)) { // Controler si la valeur numérique on affecte si non 
+
+            $this->solde = $solde;
+        }
+        
     }
 
+    //Methode toString
     public function __toString() {
-        $msg = 'Le solde de Monsieur ' .$this->nom. ' est de ' .$this->solde. ' '.self::DEVISE;
-
+        //Creer une condition 
+        if(empty($this->solde)) {
+            $msg = 'Le solde de Monsieur ' .$this->nom. 'n\'est pas correct.';
+        } else{
+            $msg = 'Le solde de Monsieur ' .$this->nom. ' est de ' .$this->solde. ' '.self::DEVISE;
+        } 
         //Retourner le message
         return $msg;
     }
